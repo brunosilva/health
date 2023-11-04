fc9942a0a5729ec0d23f81e75abd3408866b4e0f
Bruno Silva <brs.rsilva@gmail.com>
2023-11-04 14:40:01 -0300

    feat: :sparkles: share api

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Share, Text, TouchableOpacity, View } from "react-native";
 import styles from "./style";
 
 export default function ResultImc(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: `Meu imc hoje é: ${props.resultImc}`
+    })
+  }
+
   return (
     <View style={styles.resultImc}>
+      {props.resultImc !== null ?
+        <View style={styles.boxShareButton}>
+          <TouchableOpacity
+            onPress={onShare}
+            style={styles.share}
+          >
+            <Text style={styles.sharedText}>Share</Text>
+          </TouchableOpacity>
+        </View> :
+        <View />
+      }
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.numberImc}>{props.resultImc}</Text>
     </View>
